0a5357af770f1ae86e592e4e5e4e718f7c0a018f
user <user@localhost.localdomain>
2024-11-11 16:05:59 +0300

    lab4 minor changes

--- lab4/include/iterator.hpp
+++ lab4/include/iterator.hpp
@@ -16,23 +16,32 @@
 
 #include <iostream>
 
+// stride is the number of bytes from one row of pixels in memory to the next row
+// of pixels in memory
+// as if abs distance
+
+// stride is a row number
+// it's when we convert 2-D array into 1-D array: [x , y] -> a; a in [0; xy - 1]
+
 template<typename T>
+// image_iterator<T> - derived class, T - base class>
 class image_iterator : public boost::iterator_adaptor<image_iterator<T>, T>
 {
 public:
+    // w is for width, st is for stride
     image_iterator(T it, size_t w, size_t st)
-    : boost::iterator_adaptor<image_iterator<T>, T>(it)
-    , start_iterator_(it)
-    , w_(w)
-    , st_(st)
+    : boost::iterator_adaptor<image_iterator<T>, T>(it) // syntax is (Base const &iter)
+    , m_start_iterator(it)
+    , m_w(w)
+    , m_st(st)
     {}
 
     long distance_to(const image_iterator& it) const
     {
-        const auto d1 = std::abs(it.base() - it.start_iterator_) % ptrdiff_t(st_);
-        const auto d2 = std::abs(this->base() - start_iterator_) % ptrdiff_t(st_);
+        const auto d1 = std::abs(it.base() - it.m_start_iterator) % ptrdiff_t(m_st);
+        const auto d2 = std::abs(this->base() - m_start_iterator) % ptrdiff_t(m_st);
 
-        return (it.base() - d1 - (this->base() - d2)) / ptrdiff_t(st_) * w_  + (d1 - d2);
+        return (it.base() - d1 - (this->base() - d2)) / ptrdiff_t(m_st) * m_w  + (d1 - d2);
     }
 
     void advance(const int n)
@@ -43,23 +52,25 @@ public:
 
     void decrement()
     {
-        if (std::abs(this->base() - start_iterator_) % ptrdiff_t(st_) <= 0)
-            this->base_reference() -= (st_ - w_ + 1);
+        //if we go to the previous column, make sure we don't get out of bounds
+        if (std::abs(this->base() - m_start_iterator) % ptrdiff_t(m_st) <= 0)
+            this->base_reference() -= (m_st - m_w + 1);
         else
             --this->base_reference();
     }
 
     void increment() {
-        if (std::abs(this->base() - start_iterator_) % ptrdiff_t(st_) + 1 >= long(w_))
-            this->base_reference() += (st_ - w_ + 1);
+        //same in decrement, make sure we don't get out of bounds
+        if (std::abs(this->base() - m_start_iterator) % ptrdiff_t(m_st) + 1 >= long(m_w))
+            this->base_reference() += (m_st - m_w + 1);
         else
             ++this->base_reference();
     }
 
 private:
-    T start_iterator_;
-    size_t w_;
-    size_t st_;
+    T m_start_iterator;
+    size_t m_w;
+    size_t m_st;
 };
 
 template<class Container = std::vector<uint8_t>>
@@ -68,34 +79,34 @@ class image
 public:
     image(size_t width, size_t height, size_t stride):
     data(stride * height),
-    width_(width),
-    stride_(stride)
+    m_width(width),
+    m_stride(stride)
     {}
 
     auto begin()
     {
-        return image_iterator(data.begin(), width_, stride_);
+        return image_iterator(data.begin(), m_width, m_stride);
     }
 
     auto end()
     {
-        return image_iterator(data.end(), width_, stride_);
+        return image_iterator(data.end(), m_width, m_stride);
     }
 
     auto begin() const
     {
-        return image_iterator(data.begin(), width_, stride_);
+        return image_iterator(data.begin(), m_width, m_stride);
     }
 
     auto end() const
     {
-        return image_iterator(data.end(), width_, stride_);
+        return image_iterator(data.end(), m_width, m_stride);
     }
 
 private:
-    Container data; // image data including strides. Access by (x, y): data[y * stride_ + x]
-    size_t width_;
-    size_t stride_;
+    Container data; // image data including strides. Access by (x, y): data[y * m_stride + x]
+    size_t m_width;
+    size_t m_stride;
 };
 
 #endif // __ITERATOR_HPP__
